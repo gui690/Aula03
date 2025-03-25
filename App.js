da92fc74b54693b7c6e4090fcdae0e5a0f954f14
gui690 <ggr91739@gmail.com>
2025-03-25 17:51:34 -0300

    Aula03

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      
+
+
+  <View style={styles.coluna}>
+  <TouchableOpacity style={styles.view1}>
+        <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view3}>
+      <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view2}>
+      <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+  </View>
+
+
+  <View  style={styles.coluna}>
+  <TouchableOpacity style={styles.view1}>
+        <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view3}>
+      <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view2}>
+      <Image source={require('./assets/favicon.png')}/>
+      </TouchableOpacity>
+  </View>
+      
+
+
+
     </View>
   );
 }
@@ -13,8 +46,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:20,
+    columnGap: 20
+
   },
+
+  view1:{
+    //flex:1,
+  backgroundColor:'skyblue',
+  width: '100%',
+    height:200,
+    borderRadius:20,
+    justifyContent:'center',
+    alignItems:'center',
+    },
+  view2:{
+    //flex:1,
+  backgroundColor:'pink',
+  width: '100%',
+  height:200,
+  borderRadius:20,
+  justifyContent:'center',
+    alignItems:'center',
+  },
+  view3:{
+    //flex:1,
+  backgroundColor:'yellow',
+  width: '100%',
+  height:200,
+  borderRadius:20,
+  justifyContent:'center',
+    alignItems:'center',
+  },
+  coluna:{
+    width:"50%",
+    rowGap:20,
+    flexDirection:'column'
+  }
 });
